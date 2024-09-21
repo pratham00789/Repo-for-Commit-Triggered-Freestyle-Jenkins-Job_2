19e4d2c3d07e1f1aa597448c691acc6ea5868675
Pratham <119481020+pratham00789@users.noreply.github.com>
2024-09-22 00:41:51 +0530

    Update patter2.java

--- patter2.java
+++ patter2.java
@@ -2,9 +2,6 @@ public class patter2{
 
   public static void main(String args[]){
     System.out.println("hello sentence one ");
-    System.out.println("sentence two");
-    System.out.println("sentence three");
-    System.out.println("sentence four");
-    System.out.println("sentence three");
+  
   }
 }
