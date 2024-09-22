92483496c3880298adf6942ecbe32e6e713937bf
Pratham <119481020+pratham00789@users.noreply.github.com>
2024-09-22 10:43:43 +0530

    Update patter2.java

--- patter2.java
+++ patter2.java
@@ -4,7 +4,7 @@ public class patter2{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your name");
-    String name = sc.next;
+    String name = sc.next();
     System.out.println(name);
   
   }
